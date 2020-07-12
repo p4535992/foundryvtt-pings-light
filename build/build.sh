6b2d1bc1a8c7dc5729e72866f0862151e2565e39
Azzurite <azzurite@gmail.com>
2020-07-12 23:03:47 +0200

    Add gitlab ci build

---
+++ build/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -o errexit
+set -o xtrace
+
+module=$(jq -r ".name" ${base}/package.json)
+base=$(pwd)
+rm -rf dist
+mkdir -p dist/${module}
+cd dist
+cp -r ${base}/src/* ${module}
+cp -r ${base}/resources/languages ${module}
+find ${module} -name "*.test.js" -type f -delete
+
+version=${CI_COMMIT_BRANCH}||${CI_COMMIT_TAG}
+baseUrl="https://gitlab.com/foundry-azzurite/s3-alternatives/-/jobs/artifacts/${version}/raw/dist/"
+suffix="?job=build"
+distributionJson="{manifest: \"${baseUrl}${module}/module.json${suffix}\", download: \"${baseUrl}${module}.zip${suffix}\"}"
+jq -s "(.[0] | { name, description, author, version }) * .[1] * ${distributionJson}" ${base}/package.json ${base}/resources/module.json > ${module}/module.json
+
+zip ${module}.zip -r ${module}
+cd -
