911b6d718adf461721914bf4a72a5053571c8e8d
Azzurite <azzurite@gmail.com>
2020-07-13 21:01:53 +0200

    Fix wrong project name

--- build/build.sh
+++ build/build.sh
@@ -12,7 +12,7 @@ cp -r ${base}/resources/languages ${module}
 find ${module} -name "*.test.js" -type f -delete
 
 version=${CI_COMMIT_BRANCH}||${CI_COMMIT_TAG}
-baseUrl="https://gitlab.com/foundry-azzurite/s3-alternatives/-/jobs/artifacts/${version}/raw/dist/"
+baseUrl="https://gitlab.com/foundry-azzurite/${module}/-/jobs/artifacts/${version}/raw/dist/"
 suffix="?job=build"
 distributionJson="{manifest: \"${baseUrl}${module}/module.json${suffix}\", download: \"${baseUrl}${module}.zip${suffix}\"}"
 jq -s "(.[0] | { name, description, author, version }) * .[1] * ${distributionJson}" ${base}/package.json ${base}/resources/module.json > ${module}/module.json
