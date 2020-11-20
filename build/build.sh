187bc717b055d98f184be35decbe68e02fb46c5e
Azzurite <azzurite@gmail.com>
2020-11-20 14:14:52 +0100

    Fix ci build link

--- build/build.sh
+++ build/build.sh
@@ -11,8 +11,8 @@ cp -r ${base}/src/* ${module}
 cp -r ${base}/resources/languages ${module}
 find ${module} -name "*.test.js" -type f -delete
 
-version=${CI_COMMIT_BRANCH}||${CI_COMMIT_TAG}
-baseUrl="https://gitlab.com/foundry-azzurite/${module}/-/jobs/artifacts/${version}/raw/dist/"
+build_ref=${CI_BUILD_REF_NAME}
+baseUrl="https://gitlab.com/foundry-azzurite/${module}/-/jobs/artifacts/${build_ref}/raw/dist/"
 suffix="?job=build"
 distributionJson="{manifest: \"${baseUrl}${module}/module.json${suffix}\", download: \"${baseUrl}${module}.zip${suffix}\"}"
 jq -s "(.[0] | { name, description, author, version }) * .[1] * ${distributionJson}" ${base}/package.json ${base}/resources/module.json > ${module}/module.json
