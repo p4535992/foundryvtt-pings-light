f078b66343eb06784b7781c14b1230ac4818d846
p4535992 <tentimarco0@gmail.com>
2023-02-27 10:10:21 +0100

    initial sync

--- karma.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(config) {
-	config.set({
-		frameworks: [`jasmine`],
-		files: [
-			{pattern: `src/**/*.test.js`, type: `module`},
-			{pattern: `src/**/*.js`, included: false, type: `module`}
-		]
-	});
-};
